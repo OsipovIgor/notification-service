9362dc40030fb13567fb1b097ae63220f281e3cd
osipov <osipov@astralnalog.ru>
2017-03-28 15:56:49 +0300

    test telegram bot

--- api/api.js
+++ api/api.js
@@ -10,8 +10,32 @@ import SocketIo from 'socket.io';
 
 const pretty = new PrettyError();
 const app = express();
-
 const server = new http.Server(app);
+
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = "363867335:AAGPxRQwK9c6MdhRG9qB2kj5v8WxEkUDu1Q";
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  console.log("msg", msg);
+  // send a message to the chat acknowledging receipt of their message
+   bot.sendMessage(chatId, `Иди-ка ты на хуй ${msg.from.first_name} ${msg.from.last_name}`);
+});
 
 const io = new SocketIo(server);
 io.path('/ws');
@@ -83,7 +107,8 @@ if (config.apiPort) {
       messageBuffer[messageIndex % bufferSize] = data;
       messageIndex++;
       io.emit('msg', data);
-      console.log('data', data);
+      const chatId = -1001066969904;
+      bot.sendMessage(chatId, data.text);
     });
   });
   io.listen(runnable);
